87d1ea6f5d297a0f54fe2f3f0b27d1532cbe8737
Dip-kumar <makerofscience@gmail.com>
2021-12-13 07:09:23 +0530

    Update list-nav.js

--- www/list-nav.js
+++ www/list-nav.js
@@ -1,5 +1,3 @@
-<script>
 function myFunction() {
   document.getElementById("nav1").innerHTML = "<a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com'>Home</a> <a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/computer-science.html'>Computer Science</a> <a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/books.html'>Books</a> <a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/downloads.html'>Downloads</a> <a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/about-us.html'>About Us</a><a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/privacy-policy.html'>Privacy policy</a><a class='mos123-bar-item mos123-button' href='https://www.makerofscience.com/p/teachers.html'>Teachers</a>";
 }
-</script>
